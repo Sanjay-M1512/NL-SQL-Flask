7020017fedb19ea089b4b14f502a86fbb4604591
Sanjay M <167982926+Sanjay-M1512@users.noreply.github.com>
2025-06-20 07:46:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ users_collection = db['users']
 def create_mysql_database(root_user, root_password, database_name):
     try:
         connection = mysql.connector.connect(
-            host="localhost",
+            host="127.0.0.1",
             user=root_user,
             password=root_password
         )
