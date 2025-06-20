12824b1a314a75dd912c5435306e5ab67c080c0e
Sanjay M <167982926+Sanjay-M1512@users.noreply.github.com>
2025-06-20 07:47:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def execute_query():
         sql_query = re.sub(r'\s+', ' ', sql_query).strip()
 
         connection = mysql.connector.connect(
-            host="localhost",
+            host="127.0.0.1",
             user=root_user,
             password=root_password,
             database=database_name
